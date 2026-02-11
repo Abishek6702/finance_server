ab6869e817941176be48c6163dc65a6e97cbdc0e
Abishek6702 <abishekkrishnat@gmail.com>
2026-02-11 15:05:24 +0530

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://finance-client-olive.vercel.app"];
 
 app.use(
   cors({
